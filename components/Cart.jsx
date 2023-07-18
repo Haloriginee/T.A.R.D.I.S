75acd5b221e467bf9d26a583ea9bf33321f0080c
Kevin Hang <herezy2006@gmail.com>
2023-07-18 16:40:10 +0200

    cart fully functional at last

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,7 +9,7 @@ import toast from "react-hot-toast";
 const Cart = () => {
 
   const cartRef = useRef();
-  const { cartItems, totalQuantities, totalPrice, setShowCart } = useStateContext();
+  const { cartItems, totalQuantities, totalPrice, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
 
   return (
     <div className='cart-wrapper' ref={cartRef} >
@@ -49,17 +49,78 @@ const Cart = () => {
       )}
 
       <div className='product-container'>
-        {cartItems.length >= 1 && cartItems.map((item, index) => (
+        {cartItems.length >= 1 && cartItems.map((item) => (
           <div className='product' key={item._id}>
             <img
               src={urlFor(item?.image[0])}
               alt={item.name}
               className='cart-product-image'
             />
+            <div className='item-desc'>
+              <div className='flex top'>
+                <h5>
+                  {item.name}
+                </h5>
+                <h4>
+                  ${item.price}
+                </h4>
+              </div>
+              <div className='flex bottom'>
+                <div>
+                  <p className='quantity-desc'>
+                    <span
+                      className='minus'
+                      onClick={() => toggleCartItemQuantity(item._id, "dec")}
+                      >
+                      <AiOutlineMinus />
+                    </span>
+                    <span
+                      className='num'
+                      onClick=""
+                      >
+                      {item.quantity}
+                    </span>
+                    <span
+                      className='plus'
+                      onClick={() => toggleCartItemQuantity(item._id, "inc")}
+                      >
+                      <AiOutlinePlus />
+                    </span>
+                  </p>
+                </div>
+                <button
+                  type='button'
+                  className='remove-item'
+                  onClick={() => onRemove(item)}
+                >
+                  <TiDeleteOutline />
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-
+        {cartItems.length >= 1 && (
+          <div className='cart-bottom'>
+            <div className='total'>
+              <h3>
+                Subtotal:
+              </h3>
+              <h3>
+                ${totalPrice}
+              </h3>
+            </div>
+            <div className='btn-container'>
+              <button
+                type='button'
+                className='btn'
+                onClick=""
+              >
+                Pay Now
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- context/StateContext.js
+++ context/StateContext.js
@@ -6,9 +6,12 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
 
   const onAdd = ( product, quantity ) => {
 
@@ -20,7 +23,7 @@ export const StateContext = ({ children }) => {
     if(exist) {
 
       const updatedCartItems = cartItems.map((cartProduct) => {
-        if(cartProduct._id === product._id) return{
+        if(cartProduct._id === product._id) return {
           ...cartProduct,
           quantity: cartProduct.quantity + quantity,
         }
@@ -41,9 +44,43 @@ export const StateContext = ({ children }) => {
 
   const decreaseQuantity = () => {
     setQty((prevQty) => {
-      if(prevQty - 1 < 1) return1;
+      if(prevQty - 1 < 1) return 1;
       return prevQty - 1;
     });
+  }
+
+  const toggleCartItemQuantity = (id, value) => {
+
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if(value === "inc") {
+
+      setCartItems([ ...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+
+    } else if(value === "dec") {
+
+      if(foundProduct.quantity > 1) {
+
+        setCartItems([ ...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+
+      }
+    }
+  }
+
+  const onRemove = (product) => {
+
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
   }
 
   return (
@@ -62,6 +99,8 @@ export const StateContext = ({ children }) => {
         increaseQuantity,
         decreaseQuantity,
         onAdd,
+        toggleCartItemQuantity,
+        onRemove,
       }}
     >
       {children}
